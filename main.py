207335876a4c852d7dd602ef4320c3d8876d9c52
isaacaisha <medusadbt@gmail.com>
2023-08-04 09:25:54 +0200

    Fix issues for Heroku deployment

--- main.py
+++ main.py
@@ -15,10 +15,10 @@ app = Flask(__name__)
 
 # Connect to Database
 # Use Heroku Config Var for Database URL
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'sqlite:///cafes.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = 'your_secret_key_here'
 db = SQLAlchemy(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///cafes.db')
 csrf = CSRFProtect(app)
 
 
