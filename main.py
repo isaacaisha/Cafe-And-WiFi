05562fc1eb755e313f452ddfff4e0e8be46dfd10
isaacaisha <medusadbt@gmail.com>
2023-08-07 03:14:14 +0200

    trying to fix some crach on the database

--- main.py
+++ main.py
@@ -15,10 +15,10 @@ from forms import (RegistrationForm, LoginForm, SearchCafeForm,
                    UpdateCafePriceForm, AddCafeForm, DeleteCafeForm, DeleteUserForm)
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'sqlite:///cafes.db')
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')  # add to use SQlite: (, 'sqlite:///cafes.db')
 
 # CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///cafes.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')  # add to use SQlite: (, 'sqlite:///cafes.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 csrf = CSRFProtect(app)
