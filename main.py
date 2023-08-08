5d4a5e4cbfab6e1694713652d94215685cc89f3c
isaacaisha <medusadbt@gmail.com>
2023-08-09 00:38:54 +0200

    fixing

--- main.py
+++ main.py
@@ -21,9 +21,7 @@ app.config['WTF_CSRF_SECRET_KEY'] = app.config['SECRET_KEY']
 
 
 # CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = ('postgres://zszayjwntszzix'
-                                         ':3160385dacb549f3276c870e966143e209c569f4e7e999bc432c84cf6a53eb60@ec2-34'
-                                         '-202-127-5.compute-1.amazonaws.com:5432/d1b3frs5ggdt4l')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///cafes.db')
 
 # For SQLite (comment out the PostgreSQL URI)
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cafes.db'
