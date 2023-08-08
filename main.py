8c16a600bbc32a226df77070e7f76015d8cdfa7b
isaacaisha <medusadbt@gmail.com>
2023-08-08 09:16:16 +0200

    trying to connect to Postgr db

--- main.py
+++ main.py
@@ -18,12 +18,12 @@ import logging
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = 'SECRET_KEY'
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 # Set the WTF_CSRF_SECRET_KEY
 app.config['WTF_CSRF_SECRET_KEY'] = app.config['SECRET_KEY']
 
 # CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cafes.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///cafes.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 csrf = CSRFProtect(app)
